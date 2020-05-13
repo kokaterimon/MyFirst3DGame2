c6220d81591a306887e0ef90bfd8314b888e3a3b
kokaterimon <bpaucarm@outlook.com>
2020-05-13 08:02:34 -0500

    Haciendo el tutorial 12

---
+++ Assets/Coin/Coin.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class Coin : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("El juego ha comenzado");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log("Estamos en el update");
+    }
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("Algo ha colisionado con la moeda");
+    }
+}
