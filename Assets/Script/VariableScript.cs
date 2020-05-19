fc3348443721b664644a10c458c08cdca566c8d4
kokaterimon <bpaucarm@outlook.com>
2020-05-19 08:10:10 -0500

    caciendo el tutorial 27

---
+++ Assets/Script/VariableScript.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VariableScript : MonoBehaviour{
+
+
+    /*
+     
+     SECCIÓN DE VARIABLES 
+      
+    */
+    public int myNumber = 30;
+    public int total;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        total = myNumber - 5;// total = 25
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
